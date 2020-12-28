75e39b19a4b16d88a2fca3766bf623bf83db83ac
Jana Zrayka <janjoune.97@hotmail.com>
2020-12-28 17:42:00 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 // Code your solution here
 
 function findMatching(drivers,str){
-  for(let i=0;i<drivers.length;i++)
-  return drivers[i];
+ let d = drivers.find(driver => )
 }
 
 function fuzzyMatch(){
