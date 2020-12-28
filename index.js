0b9bbfaaa3299218711fed0d743e56494037953d
Jana Zrayka <janjoune.97@hotmail.com>
2020-12-28 17:40:25 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Code your solution here
+
+function findMatching(drivers,str){
+  for(let i=0;i<drivers.length;i++)
+  return drivers[i];
+}
+
+function fuzzyMatch(){
+  
+}
+
+function matchName() {
+  
+}
